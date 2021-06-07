5f65dba4d6aef638e0465a44839db1b39d7b7d96
sydneymurray <sydney_i_murray@yahoo.co.uk>
2021-06-08 00:04:50 +0100

    Files Broken Down

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ import Header from "./components/Header.js"
 import NavLeftMenu from "./components/NavLeftMenu.js"
 import Main from "./components/Main.js"
 
-
-import flaticonWelcomeImage from './assets/images/flaticon-welcome-image.png'
-import downloadButton from './assets/icons/download-button.png'
-import rateStarButton from './assets/icons/rate-star-button.png'
-import rubbishButton from './assets/icons/rubbish-bin-delete-button.png'
-
 function App() {
   return (
     <div className="app">
